9f02a5d704ccfee22a2ff949b680415dd822c5a3
dave5623 <dave5623@yahoo.com>
2020-04-27 12:31:13 -0400

    Unzip gophish into a directory

--- ServerSetup.sh
+++ ServerSetup.sh
@@ -529,7 +529,7 @@ setupSSH(){
 function Install_GoPhish {
 	apt-get install unzip > /dev/null 2>&1
 	wget https://github.com/gophish/gophish/releases/download/v0.9.0/gophish-v0.9.0-linux-64bit.zip
-	unzip gophish-v0.9.0-linux-64bit.zip
+	unzip gophish-v0.9.0-linux-64bit.zip -d gophish
 	cd gophish
         sed -i 's/"listen_url" : "127.0.0.1:3333"/"listen_url" : "0.0.0.0:3333"/g' config.json
 	read -r -p "Do you want to add an SSL certificate to your GoPhish? [y/N] " response
